8dd91bc048d13615e228b3e88149d3fba5ade23b
Ezequiel Gnich <ezequielgnich@live.com>
2020-04-18 19:28:45 -0300

    Primeiras configurações do projeto (next, eslint, postcss e tailwindcss)

---
+++ pages/_app.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../styles/styles.css";
+
+const App = ({ Component, pageProps }) => {
+  return (
+    <div>
+      <Component {...pageProps} />
+    </div>
+  );
+};
+
+export default App;
---
+++ pages/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Index = () => {
+  return (
+    <div>
+      <header className="bg-gray-200">
+        <h1>
+          <img
+            className="h-24"
+            src="/logo.png"
+            alt="Logotipo FSL"
+            height="60"
+          />
+        </h1>
+      </header>
+    </div>
+  );
+};
+
+export default Index;
