08edb2ab49d630c04a2516c4b698bad55d1745c6
Ezequiel Gnich <ezequielgnich@live.com>
2020-04-19 20:32:58 -0300

    Adicionado Componentes Header, footer e alterado a tag main.

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,16 @@
 import React from "react";
 import "../styles/styles.css";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 const App = ({ Component, pageProps }) => {
   return (
     <div>
-      <Component {...pageProps} />
+      <Header />
+      <main className="min-h-screen container mx-auto">
+        <Component {...pageProps} />
+      </main>
+      <Footer />
     </div>
   );
 };
