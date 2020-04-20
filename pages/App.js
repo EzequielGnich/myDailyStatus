35133d7652515ed37a3f4a1424e3bbf9a5cb6ea8
Ezequiel Gnich <ezequielgnich@live.com>
2020-04-20 19:39:15 -0300

    Refatoração de código app.js

--- pages/App.js
+++ pages/App.js
@@ -1,19 +1,37 @@
 import React, { useEffect } from "react";
 import auth0 from "../lib/auth0";
 import router from "next/router";
-import { db } from "../lib/db";
+
+import { checkExists, findCheckNearbyChekin } from "../lib/markers";
 
 const App = (props) => {
   useEffect(() => {
-    if (!props.isAuth) router.push("/");
+    if (!props.isAuth) {
+      router.push("/");
+    } else if (props.forceCreate) {
+      router.push("/create-status");
+    }
   });
 
-  if (!props.isAuth) return null;
+  if (!props.isAuth || props.forceCreate) return null;
 
   return (
     <div>
-      <h1>App</h1>
-      <pre>{JSON.stringify(props, null, 2)}</pre>
+      <h1 className="my-4 text-center font-bold font-black text-2xl">
+        Status próximos a você:
+      </h1>
+      <table>
+        {props.checkins.map((checkin) => {
+          return (
+            <tr key={checkin.id}>
+              <td>{checkin.id === props.user.sub ? "Seu Status" : ""}</td>
+              <td>{checkin.status}</td>
+              <td>{JSON.stringify(checkin.coords)}</td>
+              <td>{checkin.distance}</td>
+            </tr>
+          );
+        })}
+      </table>
     </div>
   );
 };
@@ -22,20 +40,31 @@ export default App;
 
 export async function getServerSideProps({ req }) {
   const session = await auth0.getSession(req);
+
   if (session) {
-    const todayCheckin = await db
-      .collection("markers")
-      .doc("2020-04-19")
-      .collection("checks")
-      .doc(session.user.sub)
-      .get();
+    const todaysData = await checkExists(session.user.sub);
+    // Variavel para forçar o usuario a criar uma postagem
+    let forceCreate = true;
 
-    console.log(todayCheckin.data());
+    if (todaysData) {
+      forceCreate = false; // Quando false o usuário não é necessário criar uma postagem
 
+      // Função para
+      const checkinsList = await findCheckNearbyChekin(todaysData);
+      return {
+        props: {
+          isAuth: true,
+          user: session.user,
+          forceCreate: false,
+          checkins: checkinsList,
+        },
+      };
+    }
     return {
       props: {
         isAuth: true,
         user: session.user,
+        forceCreate,
       },
     };
   }
