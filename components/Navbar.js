d3049c24cabc28db1b3a76390fe8c3f65eafb349
Ezequiel Gnich <ezequielgnich@live.com>
2020-04-20 22:08:03 -0300

    Refatoração de código para mostrar botões caso o usuário esteja logado ou não

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,20 +1,33 @@
 import React from "react";
 
 import NavLink from "./parts/NavLink";
+import { useAuth } from "../lib/authContext";
 
 const Navbar = () => {
+  const auth = useAuth();
+  console.log(auth.user.name);
   return (
     <div>
       <nav className="bg-gray-500 py-4 text-center shadow-xl">
+        {auth.isAuth && <NavLink href="/app">Pagina Inicial</NavLink>}
         <NavLink href="/about">Sobre</NavLink>
-        <NavLink href="/register">Cadastro</NavLink>
-        <NavLink href="/login">Entrar</NavLink>
-        <a
-          href="/api/logout"
-          className="bg-red-500 hover:bg-red-700 duration-500 text-white font-bold py-3 px-4 rounded-full"
-        >
-          Logout
-        </a>
+        {auth.isAuth && <NavLink href="/register">Cadastro</NavLink>}
+        {auth.isAuth && (
+          <a
+            href="/api/logout"
+            className="bg-red-500 hover:bg-red-700 duration-500 text-white font-bold py-3 px-4 rounded-full"
+          >
+            Logout {auth.user.name}
+          </a>
+        )}
+        {!auth.isAuth && (
+          <a
+            href="/api/login"
+            className="bg-green-500 hover:bg-green-700 duration-500 text-white font-bold py-3 px-4 rounded-full"
+          >
+            Login
+          </a>
+        )}
       </nav>
     </div>
   );
