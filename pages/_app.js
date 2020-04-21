9f0466774e9b67473b8784600057b701c26a4e64
Ezequiel Gnich <ezequielgnich@live.com>
2020-04-20 22:10:41 -0300

    Definido AuthProvider para o aplicativo

--- pages/_app.js
+++ pages/_app.js
@@ -2,16 +2,19 @@ import React from "react";
 import "../styles/styles.css";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import { AuthProvider } from "../lib/authContext";
 
 const App = ({ Component, pageProps }) => {
   return (
-    <div>
-      <Header />
-      <main className="min-h-screen container mx-auto">
-        <Component {...pageProps} />
-      </main>
-      <Footer />
-    </div>
+    <AuthProvider>
+      <div>
+        <Header />
+        <main className="min-h-screen container mx-auto">
+          <Component {...pageProps} />
+        </main>
+        <Footer />
+      </div>
+    </AuthProvider>
   );
 };
 
