3b3dbf8d766b5360a0b77795e58fd46633ea59d1
Ezequiel Gnich <ezequielgnich@live.com>
2020-04-21 18:02:03 -0300

    About.js

--- pages/About.js
+++ pages/About.js
@@ -1,7 +1,33 @@
 import React from "react";
 
 const About = () => {
-  return <h1>Sobre</h1>;
+  return (
+    <div className="w-full flex flex-col mt-12">
+      <div className="w-1/2">
+        <h1 className="font-bold text-3xl">Ezequiel Gnich</h1>
+      </div>
+      <div className="w-1/2 mt-12">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin pharetra,
+        ex nec dictum vulputate, augue nibh consequat nisi, quis eleifend urna
+        lacus non tortor. Aliquam id libero leo. Nullam scelerisque, augue ac
+        cursus feugiat, quam nibh dapibus augue, non sodales mauris tellus eu
+        risus. Proin commodo velit nec quam euismod consequat. Nam diam elit,
+        pulvinar non elit eu, dignissim dictum arcu. Proin maximus purus nec est
+        blandit, ac imperdiet risus efficitur. Donec luctus vel nunc sit amet
+        consequat. Vivamus venenatis arcu eu urna imperdiet sagittis. Sed
+        dapibus arcu vel faucibus consequat. Suspendisse sagittis enim ac ante
+        congue commodo. Nulla sed ultricies nunc. Proin congue molestie elit et
+        interdum. Vestibulum id luctus lectus. Phasellus maximus eros eget
+        vehicula semper.
+        <br />
+        <div className="mt-8">
+          <small className="float-right font-serif">Ezequiel Gnich</small>
+          <br />
+          <small className="float-right">21/04/2020</small>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default About;
